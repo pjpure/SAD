40a4e589cdca10518b83f65f5be2bcba58b751da
pjpure <pure254326@gmail.com>
2021-10-03 01:54:22 +0700

    add lab6 lsp

---
+++ Lab6 SOLID/lsp/Square.java
@@ -0,0 +1,13 @@
+package lsp;
+
+public class Square extends Rectangle {
+
+    Square(int side) {
+        this.setSide(side);
+    }
+
+    public void setSide(int side) {
+        super.setWidth(side);
+        super.setHeight(side);
+    }
+}
