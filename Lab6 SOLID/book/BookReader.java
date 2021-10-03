9ef5d99956614ea7dd8fd33de90a7ec6aa167043
pjpure <pure254326@gmail.com>
2021-10-03 22:48:37 +0700

    add book

---
+++ Lab6 SOLID/book/BookReader.java
@@ -0,0 +1,10 @@
+package book;
+
+import java.util.List;
+
+public class BookReader {
+    public static void main(String[] args) {
+        Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
+        book.printToScreen();
+    }
+}
